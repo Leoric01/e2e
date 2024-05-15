8b23ef09b91ad08b189ce751f61fee2a57bc227f
Leoric01 <troglodit.nick@seznam.cz>
2024-05-15 16:45:51 +0200

    setup entities user and role

---
+++ src/main/java/com/leoric/e2e/registration/RegistrationRequest.java
@@ -0,0 +1,15 @@
+package com.leoric.e2e.registration;
+
+import com.leoric.e2e.user.Role;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class RegistrationRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private List<Role> roles;
+}
---
+++ src/main/java/com/leoric/e2e/user/IUserService.java
@@ -0,0 +1,16 @@
+package com.leoric.e2e.user;
+
+import com.leoric.e2e.registration.RegistrationRequest;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IUserService {
+    List<User> getAllUsers();
+    User registerUser(RegistrationRequest registrationRequest);
+    Optional<User> findUserByEmail(String email);
+    void deleteUserById(int id);
+    void addUser(User user);
+    void updateUser(User user);
+
+}
